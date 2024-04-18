2b7bff5f8c25c1d0fa601cefcc54e231fd41923a
Louie Cervantes <louiecervantes@wvsu.edu.ph>
2024-04-18 15:22:30 +0800

    Save

--- HAR.py
+++ HAR.py
@@ -1,1 +1,156 @@
 import streamlit as st
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+plt.style.use('seaborn-whitegrid')
+sns.set_style("white")
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import TimeDistributed
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers import ConvLSTM2D
+from keras.utils import to_categorical
+from keras import backend as K 
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.models import load_model, model_from_json
+from keras.metrics import CategoricalAccuracy, CategoricalCrossentropy
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import confusion_matrix, auc, roc_curve, roc_auc_score, precision_score, recall_score, f1_score, accuracy_score, classification_report
+
+from numpy.random import seed
+from tensorflow.random import set_seed
+
+import os
+for dirname, _, filenames in os.walk('./UCI HAR Dataset'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+activities = {
+    1: 'Walking',
+    2: 'Walking Upstairs',
+    3: 'Walking Downstairs',
+    4: 'Sitting',
+    5: 'Standing',
+    6: 'Laying'
+}
+
+# load a single file as a numpy array
+def load_file(filepath):
+    df = pd.read_csv(filepath, header=None, delim_whitespace=True)
+    return df.values
+
+# load a list of files into a 3D array of [observations, timesteps, features(x,y,z)]
+def load_group(files, prefix=''):
+    loaded = list()
+    for f in files:
+        data = load_file(prefix + f)
+        loaded.append(data)
+    # stack group so that features are the 3rd dimension
+    loaded = np.dstack(loaded)
+    return loaded
+
+def load_dataset_group(group, prefix=''):
+    filepath = prefix + group + '/Inertial Signals/'
+    # load all 9 files as a single array
+    files = list()
+    # body acceleration
+    files += ['body_acc_x_'+group+'.txt', 'body_acc_y_'+group+'.txt', 'body_acc_z_'+group+'.txt']
+    # body gyroscope
+    files += ['body_gyro_x_'+group+'.txt', 'body_gyro_y_'+group+'.txt', 'body_gyro_z_'+group+'.txt']
+    # total acceleration
+    files += ['total_acc_x_'+group+'.txt', 'total_acc_y_'+group+'.txt', 'total_acc_z_'+group+'.txt']
+    # load input data
+    X = load_group(files, filepath)
+    # load class output
+    y = load_file(prefix + group + '/y_'+group+'.txt')
+    return X, y
+
+def load_dataset(prefix=''):
+    # load all train
+    X_train, y_train = load_dataset_group('train', prefix)
+    # load all test
+    X_test, y_test = load_dataset_group('test', prefix)
+    # zero-offset class values
+    y_train = y_train - 1
+    y_test = y_test - 1
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    s.write(f"""Dataset loaded. Training Set: X_train {X_train.shape} y_train {y_train.shape}
+        Test Set: X_test {X_test.shape} y_test {y_test.shape}""") 
+    return X_train, y_train, X_test, y_test
+
+def create_model(model):
+    classifier = KerasClassifier(model, verbose=2)
+    return classifier
+
+# GridSearch
+def grid(classifier):
+    # define the grid search parameters
+    batch_size = [8, 16, 32, 64]
+    epochs = [10, 15, 20, 25]
+    validation_split=[0.2]
+    param_grid = dict(batch_size=batch_size, epochs=epochs, validation_split=validation_split)
+    grid = GridSearchCV(estimator=classifier, param_grid=param_grid, n_jobs=-1, cv=5, return_train_score=True, verbose=2)
+    grid_result = grid.fit(X_train, y_train)
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print("%f (%f) with: %r" % (mean, stdev, param))
+    return grid_result
+
+def evaluate_model(X_train, y_train, X_test, y_test, params, model):
+    epochs, batch_size = params["epochs"], params["batch_size"]
+    es =EarlyStopping(monitor='val_loss', patience=5)
+    classifier=model()
+    history = classifier.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=2, validation_split=0.2, callbacks=[es])
+    # evaluate model
+    _, accuracy = classifier.evaluate( X_test, y_test, batch_size=batch_size, verbose=0)
+    return history, accuracy, classifier
+
+def run_model(model, grid_result):
+    history, score, classifier = evaluate_model(X_train, y_train, X_test, y_test, grid_result.best_params_, model)
+    score = score * 100.0
+    print('> %.3f' % (score))
+    results=pd.DataFrame(history.history, index=history.epoch)
+    return results, classifier
+
+def plot_loss(results):
+    plt.style.use('seaborn-whitegrid')
+    sns.set(style="white", font_scale = 1.5)
+    fig, axes = plt.subplots(1,2, figsize=(20,10), sharex=True)
+    axes[0].set_title('Loss')
+    axes[0].plot(results['loss'], label='train', )
+    axes[0].plot(results['val_loss'], label='test')
+    axes[0].legend()
+    #plot accuracy during training
+    axes[1].set_title('Accuracy')
+    axes[1].plot(results['accuracy'], label='train')
+    axes[1].plot(results['val_accuracy'], label='test')
+    axes[1].legend()
+    plt.show()
+    return
+
+def run(model):
+    classifier=create_model(model)
+    grid_result=grid(classifier)
+    results, classifier=run_model(model, grid_result)
+    plot_loss(results)
+    return classifier
+
+
+def app():
+    X_train, y_train, X_test, y_test = load_dataset(prefix="")
+    n_timesteps, n_features, n_outputs = X_train.shape[1], X_train.shape[2], y_train.shape[1]
+
+
+if __name__ == '__main__':
+    app()   
